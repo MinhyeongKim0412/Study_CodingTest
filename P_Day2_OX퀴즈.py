6b64ea690b29575599489dc5972aede066c1ecf6
MinhyeongKim <kimminhyeong9804@gmail.com>
2024-08-23 12:12:41 +0900

    Day2_For

---
+++ P_Day2_OX퀴즈.py
@@ -0,0 +1,10 @@
+#%%
+def solution(quiz):
+    result = []
+    for q in quiz:
+        left, right = q.split('=')
+        if eval(left) == int(right):
+            result.append("O")
+        else:
+            result.append("X")
+    return result
