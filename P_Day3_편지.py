9a49ec593b015895e396456c4d20000b0e1d2d6a
MinhyeongKim <kimminhyeong9804@gmail.com>
2024-08-23 12:12:50 +0900

    Day3_len

---
+++ P_Day3_편지.py
@@ -0,0 +1,16 @@
+#%%
+def solution(message):
+    count = len(message)
+    answer = count*2
+    return answer
+
+# -----------------------
+# 문구 글자 수 count (공백,특수문자 포함)
+# 편지지 가로 길이 = 문구 총 길이: count*2 (단위:cm)
+
+# <변수명>
+# 문구 message
+# 반환값(결과) answer
+
+# <사용 함수>
+# len
